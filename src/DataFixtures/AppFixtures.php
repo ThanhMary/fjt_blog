db463637da4225817961564d64eb23b908f3c129
dorianweb <37084529+dorianweb@users.noreply.github.com>
2020-11-19 20:38:01 +0100

    sa rend fou les conflits

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-<<<<<<< HEAD
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -27,18 +26,6 @@ class AppFixtures extends Fixture
             ->setPassword($this->encoder->encodePassword($product, 'user'))
             ->setRoles(['ROLE_USER']);
         $manager->persist($product);
-=======
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
-
-class AppFixtures extends Fixture
-{
-    public function load(ObjectManager $manager)
-    {
-        // $product = new Product();
-        // $manager->persist($product);
->>>>>>> 1273ea8d19d9b6911165f3266156ca443577d125
-
         $manager->flush();
     }
 }
