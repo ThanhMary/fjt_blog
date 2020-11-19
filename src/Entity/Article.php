5fbec134c24424d753fd6b05c3c7362b7934402f
dorianweb <37084529+dorianweb@users.noreply.github.com>
2020-11-19 22:42:24 +0100

    comments , likes are created need to be tested

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -51,6 +53,27 @@ class Article
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $picture_path;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="article_id")
+     */
+    private $comments;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Interaction::class, mappedBy="article_id")
+     */
+    private $interactions;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+        $this->interactions = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -137,6 +160,78 @@ class Article
     public function setPicturePath(?string $picture_path): self
     {
         $this->picture_path = $picture_path;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setArticleId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getArticleId() === $this) {
+                $comment->setArticleId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Interaction[]
+     */
+    public function getInteractions(): Collection
+    {
+        return $this->interactions;
+    }
+
+    public function addInteraction(Interaction $interaction): self
+    {
+        if (!$this->interactions->contains($interaction)) {
+            $this->interactions[] = $interaction;
+            $interaction->setArticleId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeInteraction(Interaction $interaction): self
+    {
+        if ($this->interactions->removeElement($interaction)) {
+            // set the owning side to null (unless already changed)
+            if ($interaction->getArticleId() === $this) {
+                $interaction->setArticleId(null);
+            }
+        }
 
         return $this;
     }
