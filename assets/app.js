e48fed17ff3b56e140d5925345b799f9c285b9db
Axel-AMGHAR <axel.amghar@etu.univ-amu.fr>
2020-11-23 23:54:47 +0100

    Filter author for liked articles on profile

--- assets/app.js
+++ assets/app.js
@@ -9,14 +9,57 @@
 import './styles/app.css';
 import Vue from 'vue'
 
- var app = new Vue({
+const cl = e => console.log(e);
+
+var app = new Vue({
     el: '#app',
-     delimiters: ['@{', '}'],
+    delimiters: ['@{', '}'],
     data: function(){
         return {
-            test: 'testa'
+            routes: routes,
+            filters: {
+                author: 'DESC'
+            },
+            liked_articles: [],
         }
     },
-});
+    beforeCreate: async function () {
+        await fetch(routes.profile_liked_articles)
+            .then(res => res.json())
+            .then(res => this.liked_articles = res)
+            .catch(error => alert("Erreur : " + error));
+    },
+    computed: {
+        filter_liked_articles: function () {
+            let articles = this.liked_articles;
 
-console.log('aaefaefaeffaef')
+            articles = articles.map((article, index) => {
+                article.author = index % 2 ? 'aa': 'bb';
+                return article;
+            });
+
+            /** Example alphabetical author => to remove*/
+            const sort_alphabetical = (a, b) => {
+                if(a < b) { return -1; }
+                if(a> b) { return 1; }
+                return 0;
+            }
+
+            for(let filter in this.filters){
+                if(this.filters[filter] !== null){
+                    articles.sort((article_a, article_b) =>
+                        this.filters[filter] === 'ASC' ? sort_alphabetical(article_a.author ,article_b.author) : sort_alphabetical(article_b.author ,article_a.author))
+                }
+
+            }
+            return articles;
+        }
+    },
+    methods: {
+        update_filter: function(index){
+            this.filters[index] =
+                this.filters[index] === 'ASC' ? 'DESC' : 'ASC';
+        }
+    }
+
+});
